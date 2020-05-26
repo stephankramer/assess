eb65c06601bd086756fe4f42c2d1997c37b3f95c
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-05-26 19:06:20 +0100

    Relative to docs/source apparently

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,10 +11,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
+# SCK: this is needed for readthedocs build
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
