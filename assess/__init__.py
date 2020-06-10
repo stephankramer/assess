5c798d8b961dfdbd0cf0467980e493c0e0d8cda8
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-06-10 12:05:51 +0100

    This should work.

--- assess/__init__.py
+++ assess/__init__.py
@@ -1,10 +1,10 @@
 """Assess is a python package that implements analytical solutions to the Stokes equations
 in cylindrical and spherical domains.
 """
-__all__ = ['CylindricalStokesSolutionSmoothFreeSlip', 'CylindricalStokesSolutionSmoothZeroSlip',
-           'CylindricalStokesSolutionDeltaFreeSlip', 'CylindricalStokesSolutionDeltaZeroSlip',
-           'SphericalStokesSolutionSmoothFreeSlip', 'SphericalStokesSolutionSmoothZeroSlip',
-           'SphericalStokesSolutionDeltaFreeSlip', 'SphericalStokesSolutionDeltaZeroSlip',
+__all__ = ['CylindricalStokesSolutionSmoothFreeSlip', 'CylindricalStokesSolutionSmoothZeroSlip',  # noqa:F405
+           'CylindricalStokesSolutionDeltaFreeSlip', 'CylindricalStokesSolutionDeltaZeroSlip',  # noqa:F405
+           'SphericalStokesSolutionSmoothFreeSlip', 'SphericalStokesSolutionSmoothZeroSlip',  # noqa:F405
+           'SphericalStokesSolutionDeltaFreeSlip', 'SphericalStokesSolutionDeltaZeroSlip',  # noqa:F405
            'Y', 'dYdphi', 'dYdtheta', 'to_spherical', 'from_spherical']  # noqa:F405
-from .cylindrical import *  # noqa:F403
-from .spherical import *  # noqa:F403
+from .cylindrical import *  # noqa:F403,F401
+from .spherical import *  # noqa:F403,F401
